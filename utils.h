391867240b27a1592a39a7b7411db1b0722db05b
Egor Dmitriev <egordmitriev2@gmail.com>
2017-08-27 13:18:37 +0200

    Added utils
    - Read safe line cr and crlf
    - Split string

---
+++ utils.h
@@ -0,0 +1,23 @@
+//
+// Created by egordm on 26-8-2017.
+//
+
+#ifndef OSUPP_UTILS_H
+#define OSUPP_UTILS_H
+
+
+#include <vector>
+
+namespace utils {
+    std::istream& getline(std::istream &is, std::string &t);
+
+    inline int stoi(std::string value){
+        return std::stoi(value);
+    }
+
+    std::vector<std::string> split(const std::string &str, const std::string &delimeter, const int times);
+    inline std::vector<std::string> split(const std::string &str, const std::string &delimeter) {split(str, delimeter, -1);};
+};
+
+
+#endif //OSUPP_UTILS_H
