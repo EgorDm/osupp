e86bc4fc4b03f08736704f393a046c091bbfc7cf
Egor Dmitriev <egordmitriev2@gmail.com>
2018-08-14 00:09:09 +0200

    Added example reading

--- executable/main.cpp
+++ executable/main.cpp
@@ -12,9 +12,8 @@ using namespace osupp;
 int main() {
     std::ios_base::sync_with_stdio(false);
 
-
-
-
-
+    std::string test_file = "F:\\Osu\\Songs\\295784 Yooh - LegenD\\Yooh - LegenD. (Musty) [ADVANCED].osu";
+    Beatmap bm = osupp::read_beatmap(test_file);
+    
     return 0;
 }
