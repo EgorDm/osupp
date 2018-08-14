099bb7b84d27b4b23c140276c5d3ca810c3aade0
Egor Dmitriev <egordmitriev2@gmail.com>
2018-08-14 18:48:50 +0200

    Fixed up the docs

--- docs/conf.py
+++ docs/conf.py
@@ -193,7 +193,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'osupydoc'
+htmlhelp_basename = 'osuppydoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
